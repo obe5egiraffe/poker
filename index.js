a51e48899881d888232027a61f857503be4924fd
Alyssa Quinney <alyssa@tandemmarketing.ca>
2019-09-17 13:50:36 -0600

    added match checking

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ The program must read from STDIN and write to STDOUT.
 
 make array of hands ranked lowest to highest
 go through cards and find matching
-if no pairs, HIGHCARD
+if no pairs, HIGHCARD or STRAIGHT
 if 2 match, TWOPAIR
 if 3 match, THREEOFAKIND
 5 cards in order, STRAIGHT
@@ -32,12 +32,59 @@ if 4 match, FOUROFAKIND
 get index of hand from hand array
 
 */
-const hands = ["HIGHCARD", "TWOPAIR", "THREEOFAKIND", "STRAIGHT", "FULLHOUSE", "FOUROFAKIND"];
-const cards = ["2","3","4","5","6","7","8","9","T","J","Q","K"];
+const handsRank = ["HIGHCARD", "PAIR", "TWOPAIR", "THREEOFAKIND", "STRAIGHT", "FULLHOUSE", "FOUROFAKIND"];
+const cardValues = ["2","3","4","5","6","7","8","9","T","J","Q","K"];
 
-const example = ["AAKKK 23456", "KA225 33A47"];
+let winner = "";
 
-//split example hands into arrays
+const example1 = "TTTA9";
+const example2 = "TTTT2";
+
+console.log(checkHand(example1));
+console.log(checkHand(example2));
+
+function checkHand(hand){
+	//sort hand 
+	const sortedHand = hand.split('').map(c => c[0]).sort();
+	let rank = 0;
+	//check for matches so we can narrow it down
+	let matches = [];
+	for(let i=1;i < sortedHand.length; i++){
+		if(sortedHand[i] == sortedHand[i-1]){
+			matches.push(sortedHand[i]+sortedHand[i-1]);
+		} 
+	}
+	
+	if (matches.length === 0) {
+		//Either straight or high card
+		rank = 0;
+
+	} else if(matches.length === 1){
+		rank = 1;
+
+	} else if (matches.length === 2){
+		if(matches[0] === matches[1]){
+			rank = 3;
+		} else {
+			rank = 2;
+		}
+	} else if (matches.length === 3){
+		if(matches[0] == matches[1] && matches[0] == matches[2]){
+			rank = 6;
+		} else {
+			rank = 5;
+		}
+		
+		
+	}
+	//console.log(matches);
+	return handsRank[rank];
+
+}
+
+
+
+
 
 //check for matches
 
